3f5152e4c6c41558f744c3112b46cf814ac779f5
Mustufa786 <mustufa_ayub@hotmail.com>
2019-01-08 15:01:34 +0500

    fragment added

--- app/src/main/java/detail/acad/hassannaqvi/edu/aku/academicdetailing/ui/ViewPagerActivity.java
+++ app/src/main/java/detail/acad/hassannaqvi/edu/aku/academicdetailing/ui/ViewPagerActivity.java
@@ -1,42 +1,54 @@
 package detail.acad.hassannaqvi.edu.aku.academicdetailing.ui;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 import detail.acad.hassannaqvi.edu.aku.academicdetailing.R;
 import detail.acad.hassannaqvi.edu.aku.academicdetailing.adapters.Adapter;
+import detail.acad.hassannaqvi.edu.aku.academicdetailing.databinding.ActivityViewPagerBinding;
 
 import static detail.acad.hassannaqvi.edu.aku.academicdetailing.util.Utils.vb_imgs;
 
 public class ViewPagerActivity extends AppCompatActivity {
 
+    ActivityViewPagerBinding bi;
+    Adapter adapter;
+    int lastItemPosition;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_pager);
+        bi = DataBindingUtil.setContentView(this,R.layout.activity_view_pager);
 
 
+        final int[] slides = getIntent().getIntArrayExtra("slides");
+
 //
-//        adapter = new Adapter(this, vb_imgs);
-//        bi.viewPager.setAdapter(adapter);
-//        bi.viewPager.beginFakeDrag();
-//
-//
-//        bi.nextSlide.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-////                bi.viewPager.setCurrentItem(getItem(+1), true);
-//                if (lastItemPosition == vb_imgs.length - 1) {
-//                    bi.viewPager.setCurrentItem(0);
-//                } else {
-//
-//                    bi.viewPager.setCurrentItem(getItem(+1),true);
-//                }
-//
-//
-//            }
-//        });
+        adapter = new Adapter(this, slides);
+        bi.viewPager.setAdapter(adapter);
+        bi.viewPager.beginFakeDrag();
+
+
+        bi.nextSlide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                bi.viewPager.setCurrentItem(getItem(+1), true);
+                if (lastItemPosition == slides.length - 1) {
+                    bi.viewPager.setCurrentItem(0);
+                } else {
+
+                    bi.viewPager.setCurrentItem(getItem(+1),true);
+                }
+
+
+            }
+        });
     }
+    private int getItem(int i) {
+        return bi.viewPager.getCurrentItem() + i;
+    }
+
 }
--- app/src/main/java/detail/acad/hassannaqvi/edu/aku/academicdetailing/util/Utils.java
+++ app/src/main/java/detail/acad/hassannaqvi/edu/aku/academicdetailing/util/Utils.java
@@ -7,8 +7,8 @@ import detail.acad.hassannaqvi.edu.aku.academicdetailing.R;
 public class Utils {
 
     public static final String[] childModule = new String[]{"General Danger Sign", "Cough & Difficult Breathing", "Diarrhoea", "PSBI"};
-    public static final String[] maternalModule = new String[]{"Focused Antenatal Care","Vaginal Bleeding in Pregnancy",
-            "Partograph","Pre Eclampsia & Eclampsia"," Postpartum Hemorrhage","Assessment and management of Shock"," Puerperal Sepsis"};
+    public static final String[] maternalModule = new String[]{"Focused Antenatal Care", "Vaginal Bleeding in Pregnancy",
+            "Partograph", "Pre Eclampsia & Eclampsia", " Postpartum Hemorrhage", "Assessment and management of Shock", " Puerperal Sepsis"};
 
 
     public static int[] vb_imgs = new int[]{R.drawable.vb_img_1, R.drawable.vb_img_2, R.drawable.vb_img_3,
@@ -18,5 +18,52 @@ public class Utils {
             R.drawable.vb_img_16, R.drawable.vb_img_17, R.drawable.vb_img_18, R.drawable.vb_img_19,
             R.drawable.vb_img_20, R.drawable.vb_img_21, R.drawable.vb_img_22, R.drawable.vb_img_23,
             R.drawable.vb_img_24, R.drawable.vb_img_25};
+
+    public static int[] fanc_imgs = new int[]{
+            R.drawable.fanc_05, R.drawable.fanc_06,
+            R.drawable.fanc_07, R.drawable.fanc_08,
+            R.drawable.fanc_09, R.drawable.fanc_10,
+            R.drawable.fanc_11, R.drawable.fanc_12,
+            R.drawable.fanc_13, R.drawable.fanc_14,
+            R.drawable.fanc_15, R.drawable.fanc_16,
+            R.drawable.fanc_17, R.drawable.fanc_18,
+            R.drawable.fanc_19, R.drawable.fanc_20,
+            R.drawable.fanc_21, R.drawable.fanc_22,
+            R.drawable.fanc_23, R.drawable.fanc_24,
+            R.drawable.fanc_25, R.drawable.fanc_26,
+            R.drawable.fanc_27, R.drawable.fanc_28,
+            R.drawable.fanc_29, R.drawable.fanc_30,
+            R.drawable.fanc_31, R.drawable.fanc_32,
+            R.drawable.fanc_33, R.drawable.fanc_34,
+            R.drawable.fanc_35, R.drawable.fanc_36,
+            R.drawable.fanc_37, R.drawable.fanc_38,
+            R.drawable.fanc_39, R.drawable.fanc_40,
+            R.drawable.fanc_41, R.drawable.fanc_42,
+            R.drawable.fanc_43, R.drawable.fanc_44,
+            R.drawable.fanc_45, R.drawable.fanc_46,
+            R.drawable.fanc_47, R.drawable.fanc_48,
+            R.drawable.fanc_49, R.drawable.fanc_50,
+            R.drawable.fanc_51, R.drawable.fanc_52,
+            R.drawable.fanc_53, R.drawable.fanc_54,
+            R.drawable.fanc_55, R.drawable.fanc_56};
+
+
+    public static int[] getMaternalSessions(int i) {
+
+        switch (i) {
+
+            case 0:
+                return fanc_imgs;
+
+            case 1:
+                return vb_imgs;
+
+
+
+                default:
+                    return null;
+        }
+
+    }
 
 }

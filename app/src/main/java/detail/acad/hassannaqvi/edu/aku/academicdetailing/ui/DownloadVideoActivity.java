ae88a1ca346b5f7c7af467e615bbf0519f57d6a2
ali.azaz <ali.azaz.alam@hotmail.com>
2019-03-28 17:32:34 +0500

    working on downloading video layout

---
+++ app/src/main/java/detail/acad/hassannaqvi/edu/aku/academicdetailing/ui/DownloadVideoActivity.java
@@ -0,0 +1,15 @@
+package detail.acad.hassannaqvi.edu.aku.academicdetailing.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import detail.acad.hassannaqvi.edu.aku.academicdetailing.R;
+
+public class DownloadVideoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_download_video);
+    }
+}
